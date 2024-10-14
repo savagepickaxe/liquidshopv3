50afb918f6b32d8c241f40ab43b571aab3aa54f6
unknown <assetwarmup@yahoo.com>
2024-10-14 02:02:25 -0400

    Ajout de la configuration Tailwind et débogage des styles

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  mode: 'jit', // Mode Just-In-Time pour générer les classes dynamiquement
-  purge: [
-    './**/*.liquid', // Inclure tous les fichiers Liquid
-    './**/*.html',   // Inclure tous les fichiers HTML
-    './**/*.js',     // Inclure tous les fichiers JavaScript
+  mode: 'jit',
+  content: [
+    './**/*.liquid',
+    './**/*.html',
+    './**/*.js',
   ],
   theme: {
     extend: {},
