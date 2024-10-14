ef35086e4b6a640cca365c4b252df2abd610b457
unknown <assetwarmup@yahoo.com>
2024-10-14 01:59:23 -0400

    sas

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  mode: 'jit', // Active le mode Just-In-Time de Tailwind pour générer les classes à la volée
+  mode: 'jit', // Mode Just-In-Time pour générer les classes dynamiquement
   purge: [
-    './**/*.liquid',  // Vérifie tous les fichiers Liquid
-    './**/*.html',    // Vérifie tous les fichiers HTML
-    './**/*.js',      // Vérifie tous les fichiers JavaScript
+    './**/*.liquid', // Inclure tous les fichiers Liquid
+    './**/*.html',   // Inclure tous les fichiers HTML
+    './**/*.js',     // Inclure tous les fichiers JavaScript
   ],
   theme: {
     extend: {},
-    container: {
-      center: true,
-      padding: '1rem',
-    },
   },
   plugins: [],
-}
+};
